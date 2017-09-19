c54d39c5fcf7bfca45ff6bff5cb636b55d211a89
SchruteFarms23 <romanmullady@gmail.com>
2017-09-19 09:05:11 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,36 @@
 // Code EyesOnMe Component Here
+// import React from 'react';
+
+// export default class EyesOnMe extends React.Component{
+//   onFocus = () => console.log('Good!')
+//
+//   onBlur = () => console.log('Hey! Eyes on me!')
+//   render(){
+//     return(
+//       <button onFocus={this.onFocus} onBlur={this.onBlur}> Eyes here buddy </button>
+//     );
+//   }
+// }
+
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+
+  Focus(){
+    console.log('Good!')
+  }
+
+  Blur(){
+    console.log('Hey! Eyes on me!')
+  }
+
+  render() {
+    return (
+      <button onFocus={this.Focus} onBlur={this.Blur}>
+        Eyes on me, please!
+      </button>
+    )
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,25 @@
 // Code Keypad Component Here
+import React from 'react';
+
+
+export default class Keypad extends React.Component{
+  handleInputPassword = () => console.log('Entering password...')
+  render(){
+    return (
+      <div>
+      <input type="password" onKeyUp={this.handleInputPassword}/>
+      </div>
+    );
+  }
+
+}
+
+
+
+
+
+// Mr. Burns has requested us to build a new keypad component for the nuclear plant, since the last one was way too complicated for his employees to use. We'll keep things super simple instead, and use an <input type="password" /> field to capture input. Here's how to complete the exercise:
+// In the components/Keypad.js file, create a Keypad React component.
+// In that component, render an input with the right type.
+// On that input, add an event handler that listens for the keyUp event.
+// When that event fires, use console.log to print out the text 'Entering password...'.
